b8b5b7d6166f741bd120ca8af66c484bf667a97c
William Sebastian <okeefesebastian91@gmail.com>
2025-05-14 16:27:34 +0900

    create form partials

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Cleaning out the DB...'
+Restaurant.destroy_all
+
+puts "Creating restaurants...."
+
+5.times do
+  # inside of the seeds, you can use Pet.create! to stop the seeds if it fails
+  Restaurant.create!(
+    name: Faker::Restaurant.unique.name,
+    category: Restaurant::CATEGORIES.sample,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+end
+
+puts "... created #{Restaurant.count} restaurants."
